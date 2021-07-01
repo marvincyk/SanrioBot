091376da2af48764f99b2fde967875c2a35987aa
Marvin Chew <marvincyk77@gmail.com>
2021-07-01 11:21:14 +1000

    replace ping command with !sanrio command

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ dotenv.config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const { prefix } = require("./config.json");
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+  if (msg.content === `${prefix}sanrio`) {
+    msg.channel.send(
+      "Hi, my name is SanrioBot. I'm currently being developed by Marvin. See you soon! 🌈✨"
+    );
   }
 });
 
