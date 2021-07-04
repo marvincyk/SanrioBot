24b251b62b54a94b251e09bda4288c39b2bbdac6
Marvin Chew <marvincyk77@gmail.com>
2021-07-04 13:04:26 +1000

    refactor command handler

--- events/message.js
+++ events/message.js
@@ -4,20 +4,22 @@ module.exports = {
     const prefix = process.env.PREFIX;
     if (!msg.content.startsWith(`${prefix}sanrio`) || msg.author.bot) return;
 
-    const command = msg.content.split(/ +/)[1];
+    const commandName = msg.content.split(/ +/)[1];
 
-    if (!command) {
+    if (!commandName) {
       return msg.channel.send(
         "Hi, my name is SanrioBot. I'm currently being developed by Marvin. See you soon! 🌈✨"
       );
     }
 
-    if (!client.commands.has(command)) {
+    if (!client.commands.has(commandName)) {
       return;
     }
 
+    const command = client.commands.get(commandName);
+
     try {
-      client.commands.get(command).execute(msg);
+      command.execute(msg);
     } catch (err) {
       console.error(err);
       msg.reply("there was an error trying to execute that command!");
