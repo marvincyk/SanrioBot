d234cedeccf4177ba735a0fcf09621601142756e
Marvin Chew <marvincyk77@gmail.com>
2021-07-04 11:35:18 +1000

    event handler

---
+++ events/message.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: "message",
+  execute(msg) {
+    if (!msg.content.startsWith(`${prefix}sanrio`) || msg.author.bot) return;
+
+    const command = msg.content.split(/ +/)[1];
+
+    if (!command) {
+      return msg.channel.send(
+        "Hi, my name is SanrioBot. I'm currently being developed by Marvin. See you soon! 🌈✨"
+      );
+    }
+
+    if (!client.commands.has(command)) {
+      return;
+    }
+
+    try {
+      client.commands.get(command).execute(msg);
+    } catch (err) {
+      console.error(err);
+      message.reply("there was an error trying to execute that command!");
+    }
+  },
+};
